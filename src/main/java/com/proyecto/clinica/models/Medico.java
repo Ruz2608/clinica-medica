c35d1075aa5cb9ef5589f85cf0f4241569ea12d0
Ruz2608 <coffe7638@gmail.com>
2026-07-23 14:45:58 -0500

    Refactor Medico.java to improve code clarity
    Removed unnecessary comments and cleaned up imports.

--- src/main/java/com/proyecto/clinica/models/Medico.java
+++ src/main/java/com/proyecto/clinica/models/Medico.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
-// Importaciones necesarias para el blindaje
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -21,7 +21,6 @@ import lombok.EqualsAndHashCode;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-// CLAVE: Excluimos 'pacientes' para evitar el bucle infinito y el apagado
 @ToString(exclude = "pacientes")
 @EqualsAndHashCode(callSuper = true, exclude = "pacientes")
 public class Medico extends Persona {
@@ -31,7 +30,7 @@ public class Medico extends Persona {
     
     private String centroEstudios;
     
-    private String especialidad; // Asegúrate de tener esto si lo usas en el Front
+    private String especialidad; 
     
     private String tipoContratacion;
 
